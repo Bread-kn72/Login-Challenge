b1809a46dc9a2de528489aa52e83d61d3658fef5
Bread-kn72 <ordinarytom72@gmail.com>
2024-10-10 00:03:57 +0900

    [Feat] (MainViewController) 로그인 기능 구현

--- Login Challenge/MainViewController.swift
+++ Login Challenge/MainViewController.swift
@@ -25,13 +25,42 @@ final class MainViewController: UIViewController {
     
     // MARK: - methods
     func configureAddTarget() {
-        mainView.loginbutton.addTarget(self, action: #selector(checkMember), for: .touchUpInside)
+        mainView.loginbutton.addTarget(self, action: #selector(tappedLoginButton), for: .touchUpInside)
+        mainView.signupButton.addTarget(self, action: #selector (tappedSignUpButton), for: .touchUpInside)
     }
     
-    @objc func checkMember() {
+    @objc func tappedLoginButton() {
+        guard let email = mainView.emailTextField.text, !email.isEmpty,
+              let password = mainView.passwordTextField.text, !password.isEmpty else {
+            print("Email과 비밀번호를 입력해주세요.")
+            return
+        }
+        
+        let fetchRequest = MI.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "email == %@ AND password == %@", email, password)
+        
+        do {
+            let result = try CoreDataMethods.shared.container.viewContext.fetch(fetchRequest)
+            
+            if !result.isEmpty {
+                print("로그인 성공")
+                present(SecondViewController(), animated: true)
+            } else {
+                print("이메일 또는 비밀번호가 잘못되었습니다.")
+                let alert = UIAlertController(title: "로그인 실패", message: "이메일 또는 비밀번호가 잘못되었습니다.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "확인", style: .default))
+                present(alert, animated: true)
+            }
+            
+        } catch {
+            print("데이터 읽기 실패: \(error.localizedDescription)")
+        }
     }
     
-    func tappedLoginButton() {
+    @objc func tappedSignUpButton() {
+        let signUpVC = SignUpViewController()
+        signUpVC.modalPresentationStyle = .automatic
+        present(signUpVC, animated: true)
     }
 }
 
