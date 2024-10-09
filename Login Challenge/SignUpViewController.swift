56bda2c63a10679eed546779923e34cd3a24c5fe
Bread-kn72 <ordinarytom72@gmail.com>
2024-10-09 23:23:29 +0900

    [Feat] (SignUpViewController) signup 기능 구현

--- Login Challenge/SignUpViewController.swift
+++ Login Challenge/SignUpViewController.swift
@@ -5,4 +5,34 @@
 //  Created by Kinam on 10/9/24.
 //
 
-import Foundation
+import UIKit
+
+final class SignUpViewController: UIViewController {
+    
+    // MARK: - properties
+    private let signUpView = SignUpView()
+    
+    // MARK: - life cycle
+    override func loadView() {
+        view = signUpView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - methods
+    func configureAddTarget() {
+        signUpView.signUpButton.addTarget(self, action: #selector(tappedSignUpButton), for: .touchUpInside)
+    }
+    
+    @objc func tappedSignUpButton() {
+        if let email = signUpView.emailTextField.text, let password = signUpView.passwordTextField.text {
+            print("email: \(email), password: \(password)")
+            CoreDataMethods.shared.createData(email: email, password: password)
+        }
+        
+        self.dismiss(animated: true)
+    }
+}
