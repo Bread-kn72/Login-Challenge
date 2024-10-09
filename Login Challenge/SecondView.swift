17ef133e47a1f869618a625777b674d19f58aa0d
Bread-kn72 <ordinarytom72@gmail.com>
2024-10-09 21:51:58 +0900

    [Add] (SecondView, SecondViewController) 추가

---
+++ Login Challenge/SecondView.swift
@@ -0,0 +1,8 @@
+//
+//  SecondView.swift
+//  Login Challenge
+//
+//  Created by Kinam on 10/9/24.
+//
+
+import Foundation
---
+++ Login Challenge/SecondViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SecondViewController.swift
+//  Login Challenge
+//
+//  Created by Kinam on 10/9/24.
+//
+
+import Foundation
