da376cfcc27ac4b33927687a474f7aae04975159
Bread-kn72 <ordinarytom72@gmail.com>
2024-10-10 00:21:47 +0900

    [Feat] (SecondViewController) 탈퇴 페이지 기능 구현

--- Login Challenge/SecondViewController.swift
+++ Login Challenge/SecondViewController.swift
@@ -10,7 +10,41 @@ import UIKit
 final class SecondViewController: UIViewController {
     
     // MARK: - properties
-    private let mainView = SecondView()
+    private let secondView = SecondView()
     
+    private var email: String?
+    private var password: String?
     
+    // MARK: - life cycle
+    init(email: String? = nil, password: String? = nil) {
+        self.email = email
+        self.password = password
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = secondView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureAddTarget()
+    }
+    
+    // MARK: - methods
+    private func configureAddTarget() {
+        secondView.mcButton.addTarget(self, action: #selector (mcButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func mcButtonTapped() {
+        if let email = email, !email.isEmpty, let password = password, !password.isEmpty {
+            CoreDataMethods.shared.deleteData(email: email, password: password)
+            self.dismiss(animated: true)
+        }
+    }
 }
